d53f77fba2862347691e82745db0fed5155c22a4
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-10 11:56:26 +0530

    add Student controller with signup

---
+++ Learnify-Backend/src/controllers/student.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+
+
+const Student = require('../models/student');
+const Educator = require("../models/educator");
+const Token = require("../models/token");
+
+
+exports.userSignup = async (req, res, next) => {
+    try {
+        let user = await Student.findOne({email: req.body.email}).exec();
+
+        if (user) {
+            if (req.file)
+                deleteFile.deleteFile(req.file.path);
+            return res.status(409).json({
+                message: 'Mail is already in use - Student'
+            });
+        }
+
+
+        const hash = await bcrypt.hash(req.body.password, 10);
+
+        let profilePic = null;
+        if (req.file) {
+            profilePic = req.file.path;
+        }
+
+        const newUser = new Student({
+            fname: req.body.fname,
+            lname: req.body.lname,
+            gender: req.body.gender,
+            location: req.body.location,
+            dob: req.body.dob,
+            username: req.body.username,
+            password: hash,
+            phone: req.body.phone,
+            email: req.body.email,
+            profilePic: profilePic,
+            interests: req.body.interests,
+            bookmarkedCourses: req.body.bookmarkedCourses,
+            enrolledCourses: req.body.enrolledCourses
+        });
+
+        const result = await newUser.save();
+
+        return res.status(201).json({
+            message: 'Student created'
+        });
+    } catch (err) {
+        console.log(err);
+        if (req.file) {
+            deleteFile.deleteFile(req.file.path);
+        }
+        return res.status(500).json({
+            error: err
+        });
+    }
+};
