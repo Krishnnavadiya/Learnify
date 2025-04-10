422e2efc3db65ae023daccfa9629b6299f31d825
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-10 22:01:12 +0530

    add route for Update ResetPassword and EditProfile in Student router

--- Learnify-Backend/src/routes/student.js
+++ Learnify-Backend/src/routes/student.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const StudentController = require("../controllers/student");
 
-
 router.post("/signup", profileUpload, StudentController.userSignup);
 router.post("/login", StudentController.userLogin);
+router.post('/reset-password', StudentController.resetPassword);
+router.post('/update-password', StudentController.updatePassword);
+router.patch("/edit-profile", checkAuth, profileUpload, StudentController.userEdit);
+router.delete("/:email", checkAuth, StudentController.userDelete);
 
 
 module.exports = router;
