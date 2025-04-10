31697f541d8540b38054eb87291219ab0becc209
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-10 11:45:02 +0530

    update Token model schema and validations

--- Learnify-Backend/src/models/token.js
+++ Learnify-Backend/src/models/token.js
@@ -10,6 +10,11 @@ const tokenSchema = new Schema({
         type: String,
         required: true,
     },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: 3600,
+    },
 });
 
 module.exports = mongoose.model("token", tokenSchema);
