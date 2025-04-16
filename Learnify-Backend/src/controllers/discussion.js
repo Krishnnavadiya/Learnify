05962c073cae57e8ac0f83f28f5ebdf42d06d6ef
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-16 18:09:28 +0530

    add Discussion controller

---
+++ Learnify-Backend/src/controllers/discussion.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const Discussion = require('../models/discussion');
+const Student = require('../models/student');
+const Educator = require('../models/educator');
+const Course = require('../models/course');
+
