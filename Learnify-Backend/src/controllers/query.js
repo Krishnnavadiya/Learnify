3dc10ba924877fd1d1b1c399cbba067d469a7bf0
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-17 13:52:52 +0530

    add GetAllCourse method in Query controller

--- Learnify-Backend/src/controllers/query.js
+++ Learnify-Backend/src/controllers/query.js
@@ -8,3 +8,18 @@ const Educator = require('../models/educator');
 const Student = require('../models/student');
 const Assignment = require("../models/assignment");
 
+exports.getAllCourse = async (req, res, next) => {
+    try {
+        const courses = await Course.find();
+
+        return res.status(200).json({
+            courses: courses
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: err
+        });
+    }
+}
+
