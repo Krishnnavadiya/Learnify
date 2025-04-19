c58cdbdff8e1a6d2eb8dcadc2a61df24442a5965
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-19 12:22:29 +0530

    add Discussion Controller.test.js file

---
+++ Learnify-Backend/test/discussionController.test.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const app = require('../app');
+const Student = require('../api/models/student');
+const Educator = require('../api/models/educator');
+const Course = require('../api/models/course');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+beforeAll(async () => {
+    const testDbUrl = 'mongodb+srv://Group16:Group16@cluster0.vfhbrkw.mongodb.net/Test_Common_Ground?retryWrites=true&w=majority';
+    await mongoose.disconnect();
+    await mongoose.connect(testDbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+}, 10000);
+
+beforeEach(async () => {
+    // await Student.deleteMany();
+});
+
+afterAll(async () => {
+    // delete all collections
+    await mongoose.disconnect();
+});
+
