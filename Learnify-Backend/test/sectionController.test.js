7c945009e8ca6bd6f450c5656fff9e2083e4f9c3
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-19 12:41:22 +0530

    add sectionController.test.js file - Add Post

--- Learnify-Backend/test/sectionController.test.js
+++ Learnify-Backend/test/sectionController.test.js
@@ -250,3 +250,110 @@ describe('Section Controller - deleteSection', () => {
     });
 });
 
+describe('Section Controller - addPost', () => {
+    it('should add a post to a section', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const imagePath = path.join(__dirname, 'images.png'); // Replace with the actual path to your educator's profile picture
+        const profilePic = fs.readFileSync(imagePath);
+
+        let educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+
+        const res2 = await request(app)
+            .post(`/educator/create-section/${educator.courseCreated[0]._id}`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send({
+                title: 'testSection',
+            })
+
+        const post = {
+            title: 'testPost',
+            body: 'testContent',
+        }
+
+        educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+
+        const res1 = await request(app)
+            .post(`/educator/add-post/${educator.courseCreated[0]._id}/${educator.courseCreated[0].courseSections[0]}`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .field('title', post.title)
+            .field('body', post.body)
+            .attach('attachments', profilePic, 'profilePic.png');
+
+        expect(res1.statusCode).to.equal(201);
+        expect(res1.body.message).to.equal('Post added');
+    });
+    it('should not add a post to a section if course does not exist', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+        const post = {
+            title: 'testPost',
+            body: 'testContent',
+        }
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const res1 = await request(app)
+            .post(`/educator/add-post/123456789012345678901234/123456789012345678901234`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send(post);
+
+        expect(res1.statusCode).to.equal(404);
+        expect(res1.body.message).to.equal('Course not found');
+    });
+    it('should not add a post to a section if section does not exist', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+        const post = {
+            title: 'testPost',
+            body: 'testContent',
+        }
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+        const res1 = await request(app)
+            .post(`/educator/add-post/${educator.courseCreated[0]._id}/123456789012345678901234`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send(post);
+
+        expect(res1.statusCode).to.equal(404);
+        expect(res1.body.message).to.equal('Section not found');
+    });
+    it('should not add a post to a section if user is not the creator of the course', async () => {
+        const educator1 = {
+            email: 'testeduc1ator@example.com',
+            password: 'testPassword',
+        };
+        const post = {
+            title: 'testPost',
+            body: 'testContent',
+        }
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+        const res1 = await request(app)
+            .post(`/educator/add-post/${educator.courseCreated[0]._id}/${educator.courseCreated[0].courseSections[0]._id}`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send(post);
+
+        expect(res1.statusCode).to.equal(401);
+        expect(res1.body.message).to.equal('Unauthorized');
+    });
+});
+
