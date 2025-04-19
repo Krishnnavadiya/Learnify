f9bb87dba366d614da0c60abf7b49f02491e4cb5
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-19 12:39:27 +0530

    add sectionController.test.js file - Edit Section

--- Learnify-Backend/test/sectionController.test.js
+++ Learnify-Backend/test/sectionController.test.js
@@ -90,3 +90,88 @@ describe('Section Controller - createSection', () => {
     });
 });
 
+describe('Section Controller - editSection', () => {
+    it('should edit a section', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+        const educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const res1 = await request(app)
+            .patch(`/educator/edit-section/${educator.courseCreated[0]._id}/${educator.courseCreated[0].courseSections[0]._id}`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send({
+                title: 'editedSection',
+            })
+
+        expect(res1.statusCode).to.equal(201);
+        expect(res1.body.message).to.equal('Section edited');
+        expect(res1.body).to.have.property('section');
+    });
+    it('should not edit a section if course does not exist', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const res1 = await request(app)
+            .patch(`/educator/edit-section/123456789012345678901234/123456789012345678901234`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send({
+                title: 'editedSection',
+            })
+
+        expect(res1.statusCode).to.equal(404);
+        expect(res1.body.message).to.equal('Course not found');
+    });
+    it('should not edit if section does not exist', async () => {
+        const educator1 = {
+            email: 'testeducator@example.com',
+            password: 'testPassword',
+        };
+
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+        const res1 = await request(app)
+            .patch(`/educator/edit-section/${educator.courseCreated[0]._id}/123456789012345678901234`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send({
+                title: 'editedSection',
+            })
+
+        expect(res1.statusCode).to.equal(404);
+        expect(res1.body.message).to.equal('Section not found');
+    });
+    it('should not edit a section if user is not the creator of the course', async () => {
+        const educator1 = {
+            email: 'testeduc1ator@example.com',
+            password: 'testPassword',
+        };
+
+        const res = await request(app)
+            .post('/educator/login')
+            .send(educator1)
+
+        const educator = await Educator.findOne({email: 'testeducator@example.com'}).populate('courseCreated').exec();
+        const res1 = await request(app)
+            .patch(`/educator/edit-section/${educator.courseCreated[0]._id}/${educator.courseCreated[0].courseSections[0]._id}`)
+            .set('Authorization', 'Bearer ' + res.body.token)
+            .send({
+                title: 'editedSection',
+            })
+
+        expect(res1.statusCode).to.equal(401);
+        expect(res1.body.message).to.equal('Unauthorized');
+    });
+});
