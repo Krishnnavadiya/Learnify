ec6e6dcfcac08064a9f1c7cdcf0d39ddc74551f6
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-04-19 12:47:52 +0530

    add CreateSection Function in Section Controller

--- Learnify-Backend/src/controllers/section.js
+++ Learnify-Backend/src/controllers/section.js
@@ -11,3 +11,40 @@ const {deleteFolder} = require("../../utils/deleteFile");
 const path = require("path");
 
 
+exports.createSection = async (req, res, next) => {
+    try {
+        const course = await Course.findById(req.params.courseId).exec();
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (course.createdBy.toString() !== req.userData.userId.toString()) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        const section = new Section({
+            title: req.body.title,
+            posts: []
+        });
+
+        await section.save();
+        course.courseSections.push(section._id);
+        await course.save();
+
+        return res.status(201).json({
+            message: 'Section created',
+            sectionId: section
+        });
+    } catch
+        (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: err
+        });
+    }
+};
+
